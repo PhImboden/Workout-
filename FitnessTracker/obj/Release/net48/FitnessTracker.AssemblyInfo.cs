f186b8052bf26e47110103d914a6757de6eaace1
philippimboden06 <philippimboden06@gmail.com>
2026-04-29 08:45:42 +0200

    objectiv

--- FitnessTracker/obj/Release/net48/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Release/net48/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097ba22449f5f19624a417f134bc7ccb3578eac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
